2df590d9b142f0dbc59303339282a552907594ed
t.mozhanova <t.mozhanova@tinkoff.ru>
2021-10-18 15:30:00 +0300

    delete not needed code

--- src/main/java/itmo/rbdplab2/client/ClientCommand.java
+++ src/main/java/itmo/rbdplab2/client/ClientCommand.java
@@ -1,23 +1,8 @@
 package itmo.rbdplab2.client;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -43,27 +28,6 @@ public class ClientCommand {
         } catch (Exception e) {
             System.out.println("something goes wrong");
         }
-
-//        HttpClient httpclient = HttpClients.createDefault();
-//        HttpPost httppost = new HttpPost("http://localhost:8080/library/add");
-//
-//        List<NameValuePair> params = new ArrayList<>(6);
-//        params.add(new BasicNameValuePair("name", name));
-//        params.add(new BasicNameValuePair("author", author));
-//        params.add(new BasicNameValuePair("genre", genre));
-//        params.add(new BasicNameValuePair("annotation", annotation));
-//        params.add(new BasicNameValuePair("date", date));
-//        params.add(new BasicNameValuePair("isbn", isbn));
-//        httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-//
-//        HttpResponse response = httpclient.execute(httppost);
-//        HttpEntity entity = response.getEntity();
-//
-//        if (entity != null) {
-//            try (InputStream instream = entity.getContent()) {
-//                //System.out.println(entity);
-//            }
-//        }
     }
 
     public void findBook(String byName, String byAuthor, String byKeyWord, String value) {
